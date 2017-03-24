06219e77d8d9137d839d962c99eded0121c7b0bf
martchello <marcello_1000@yahoo.com.br>
2017-03-23 21:52:44 -0300

    Dado class done

--- src/Dado.java
+++ src/Dado.java
@@ -1,5 +1,6 @@
 public class Dado {
-	private int n; 
+	private int n, last = 1;
+	private Random d = new Random();
 
 	public Dado() {
 		this.n = 6;
@@ -10,18 +11,48 @@ public class Dado {
 	}
 
 	public int getLado() {
-
-		return 0;
+		return this.last;
 	}
 
 	public int rolar() {
+		/* If n == 6, then getIntRand will 
+		return a number between 0 and 5. Thus the +1 */
+		this.last =  d.getIntRand(n - 1) + 1;
 
-		return 0;
+		return last;
 	}
 
 	@Override
 	public String toString() {
+		if (this.last == 1) 
+			return "+-----+\n|     |\n|  *  |\n|     |\n+-----+";
+		
+		if (this.last == 2) 
+			return "+-----+\n|*    |\n|     |\n|    *|\n+-----+";
 
-		return "";
+		if (this.last == 3)
+			return "+-----+\n|*    |\n|  *  |\n|    *|\n+-----+";
+		
+		if (this.last == 4) 
+			return "+-----+\n|*   *|\n|     |\n|*   *|\n+-----+";
+		
+		if (this.last == 5) 
+			return "+-----+\n|*   *|\n|  *  |\n|*   *|\n+-----+";
+		
+		if (this.last == 6)
+			return "+-----+\n|* * *|\n|     |\n|* * *|\n+-----+";
+
+		return "" + this.last;	
 	}
 }
+
+/*
+
+ 1          2          3          4          5			6
++-----+    +-----+    +-----+    +-----+    +-----+		+-----+
+|     |    |*    |    |*    |    |*   *|    |*   *|		|* * *|
+|  *  |    |     |    |  *  |    |     |    |  *  |		|     |
+|     |    |    *|    |    *|    |*   *|    |*   *|		|* * *|
++-----+    +-----+    +-----+    +-----+	+-----+		+-----+
+
+*/
--- src/bozo.java
+++ src/bozo.java
@@ -1,6 +1,10 @@
 public class bozo {
 
 	public static void main(String[] args) {
-		
+		Dado d = new Dado();
+
+		d.rolar();
+
+		System.out.println(d);
 	}
 }
