3a252e3150a99ac923bfbb653fbae8d2d8250b1b
Psycho-Ray <alex_sander_r.silva@hotmail.com>
2017-03-24 14:34:09 -0300

    Update Placar.java
    toString still missing

--- src/Placar.java
+++ src/Placar.java
@@ -1,16 +1,28 @@
 public class Placar {
+	int scoreBoard[10];
 	
-	public Placar(){
-
+	/*Inicia com o placar zerado*/
+	public Placar() {
+		for (int i=0; i<10; i++)
+			scoreBoard[i] = 0;
 	}
 
+	/*Adiciona a pontuação correspondente ao placar, mas não checa se o movimento é honesto*/
 	public void add(int posicao, int[] dados) {
-
+		if (scoreBoard[posicao - 1] == 0) {
+			if (posicao <= 6) scoreBoard[posicao - 1] = posicao * dados.lenght;
+			else if (posicao == 7) scoreBoard[6] = 15;
+			else if (posicao == 8) scoreBoard[7] = 20;
+			else if (posicao == 9) scoreBoard[8] = 30;
+			else if (posicao == 10) scoreBoard[9] = 40;
+		}
 	}
 
+	/*Retorna a soma dos pontos no placar*/
 	public int getScore() {
-
-		return 0;
+		int sum = 0;
+		for (int i=0; i<10; i++) sum += scoreBoard[i];
+		return sum;
 	}
 
 	@Override
